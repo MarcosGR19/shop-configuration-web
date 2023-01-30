b3c2aeccb0d20938970fc92b2e43cd1ed9bdad9d
MarcosGR19 <marcos.ace98@gmail.com>
2023-01-30 01:14:34 +0100

    modificated back url

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -10,12 +10,15 @@ export class ProductService {
 
   getProducts () {
     // return this.http.get('https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products');
-    return this.http.get('http://localhost:3000/products');
+    return this.http.get('https://shop-configuration-web-backend.vercel.app/products')
+    // return this.http.get('http://localhost:3000/products');
+
   }
 
   getProduct (id:number) {
     // return this.http.get('https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products');
-    return this.http.get('http://localhost:3000/products/'+id);
+    return this.http.get('https://shop-configuration-web-backend.vercel.app/products'+id);
+    // return this.http.get('http://localhost:3000/products/'+id);
   }
 
 
