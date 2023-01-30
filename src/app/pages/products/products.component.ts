2dd2c939bf0f5e941cc7a32f3a22d857ceecfc31
MarcosGR19 <marcos.ace98@gmail.com>
2023-01-30 01:11:25 +0100

    added searcher

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,5 +1,6 @@
 import { ProductService } from './../../shared/services/product.service';
 import { Component, OnInit } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-products',
@@ -10,18 +11,22 @@ export class ProductsComponent implements OnInit{
   constructor (private productService:ProductService) {}
 
   public productList:any[] = [];
+  public productListBackup:any[] = [];
 
   public listMode:boolean=false;
 
-  ngOnInit(): void {
+  public inputText?:string='';
 
+  ngOnInit(): void {
     this.productService.getProducts().subscribe((res:any)=>{
       this.productList = res;
       for(let product of this.productList){
         product.starPerc = product.stars*100/5 + '%'
       }
-      console.log(this.productList)
+
+      this.productListBackup = [...this.productList];
     });
+    
   }
 
   changeToListMode():void{
@@ -31,4 +36,9 @@ export class ProductsComponent implements OnInit{
   changeToCatalogMode():void{
     this.listMode=false;
   }
+
+  searchValue(inputText?:any):void{
+    this.productList = this.productListBackup.filter(item=> item.name.toLowerCase().includes(inputText?.toLowerCase()));
+  }
+  
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -13,7 +14,8 @@ import { ListComponent } from './list/list.component';
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule
   ]
 })
 export class ProductsModule { }
